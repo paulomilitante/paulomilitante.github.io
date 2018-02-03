09b7061b86cdc10498dd21fc62c86ae3c55ed50f
paulomilitante <paulo.militante@gmail.com>
2018-02-03 19:59:05 +0800

    Front end edit, google map responsive

--- csp2/contact.php
+++ csp2/contact.php
@@ -27,7 +27,9 @@ function display_content() {
 
 					<p>3rd Floor Caswynn Building, No. 134 Timog Avenue, Sacred Heart, Diliman, Quezon City, 1103 Metro Manila</p></br>
 
+					<div class='googlemap-responsive'>
 					<iframe src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3860.407842056246!2d121.04160121427005!3d14.632775280211666!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3397b7afe6dd8817%3A0x1301863d86ca644!2sTuitt+Incorporated!5e0!3m2!1sen!2sph!4v1517577115232' width='600' height='450' frameborder='0' style='border:0' allowfullscreen></iframe>
+					</div>
 				</div>
 			</div>
 		</div>";	
